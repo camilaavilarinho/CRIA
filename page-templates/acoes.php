70e783a9c847bab3aeea8e3f28ced82c4157572d
Camila Vilarinho <camilaavilarinho@gmail.com>
2016-08-08 16:31:20 -0300

    Add actions page and title in the other pages

--- page-templates/acoes.php
+++ page-templates/acoes.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Projetos
+Template Name: Acoes
 */
 get_header();
 ?>
@@ -24,7 +24,7 @@ get_header();
 								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/familia.png" class="img-responsive" alt="">
 							</a>
 							<div class="portfolio-caption">
-								<h4>Família Acolhedora</h4>
+								<h4>Pintando o Sete e Reunião com Famílias</h4>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-6 portfolio-item">
@@ -37,7 +37,7 @@ get_header();
 								<img src="<?php bloginfo('stylesheet_directory');?>/images/projetos/papo.png" class="img-responsive" alt="">
 							</a>
 							<div class="portfolio-caption">
-								<h4>Escola de Família</h4>
+								<h4>Manhã de Lazer</h4>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-6 portfolio-item">
@@ -50,7 +50,7 @@ get_header();
 								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/maosdadas.png" class="img-responsive" alt="">
 							</a>
 							<div class="portfolio-caption">
-								<h4>Fortalecendo vínculos</h4>
+								<h4>Seminários Estaduais</h4>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-6 portfolio-item">
@@ -63,7 +63,20 @@ get_header();
 								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/exposicao.png" class="img-responsive" alt="">
 							</a>
 							<div class="portfolio-caption">
-								<h4>Papo da Adoção</h4>
+								<h4>Exposições Fotográficas</h4>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-6 portfolio-item">
+							<a href="#portfolioModal5" class="portfolio-link" data-toggle="modal">
+								<div class="portfolio-hover">
+									<div class="portfolio-hover-content">
+										<i class="fa fa-plus fa-3x"></i>
+									</div>
+								</div>
+								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/exposicao.png" class="img-responsive" alt="">
+							</a>
+							<div class="portfolio-caption">
+								<h4>Projeto de Mãos Dadas</h4>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-6 portfolio-item">
@@ -76,7 +89,7 @@ get_header();
 								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/grupo.png" class="img-responsive" alt="">
 							</a>
 							<div class="portfolio-caption">
-								<h4>Adoções Necessárias</h4>
+								<h4>Campanha Abandonar é Crime</h4>
 							</div>
 						</div>
 					</div>
@@ -99,9 +112,9 @@ get_header();
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2">
 					<div class="modal-body">
-						<h2>Programa Família Acolhedora</h2>
+						<h2>Pintando o Sete e Reunião com Famílias</h2>
 						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/familia.jpg" alt="">
-						<p>O <strong>Programa Família Acolhedora</strong> é um acolhimento familiar temporário de crianças e adolescentes até que estes sejam reintegrados à família de origem ou disponibilizados para adoção</p>
+						<p>O <strong>O Pintando o Sete e Reunião com Famílias </strong> é um evento que acontece na última quarta-feira de cada mês e consiste em reunir as Famílias Acolhedoras que participam do programa do Cria acolhendo crianças ou adolescentes em suas casas. No Pintando o Sete as crianças e adolescente desfrutam de atividades e brincadeiras junto a um grupo de voluntários ou estagiários. Já a Reunião das Famílias é sempre mediada por nossa equipe psicossocial, abordando temas relevantes para o convívio no acolhimento familiar.</p>
 						<a class="btn" href="<?php printf(home_url());?>/form" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Se inscreva</a>
 					</div>
 				</div>
@@ -122,9 +135,9 @@ get_header();
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2">
 					<div class="modal-body">
-						<h2>Escola de Família</h2>
+						<h2>Manhã de Lazer</h2>
 						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/papo.jpg" alt="">
-						<p>Trabalha com a rede escolar realizando ações socioeducativas com alunos e suas famílias para contribuir com o fortalecimento das relações e prevenir a ruptura dos vínculos familiares e um consequente abrigamento.</p>
+						<p>Também chamada de <strong>Manhã de Piscina</strong>, é um evento que acontece nos últimos sábados dos meses com o objetivo de proporcionar lazer e bem-estar às crianças e adolescentes que vivem em instituições de acolhimento de Teresina.</p>
 						<a class="btn" href="<?php printf(home_url());?>/form" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Se inscreva</a>
 					</div>
 				</div>
@@ -145,9 +158,9 @@ get_header();
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2">
 					<div class="modal-body">
-						<h2>Fortalecendo vínculos</h2>
+						<h2>Seminários Estaduais</h2>
 						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/maosdadas.jpg" alt="">
-						<p>Acompanhamento psicossocial às famílias de origem das crianças e adolescentes inseridos nos programas visando sua reintegração, sempre que possível, e continuidade do acompanhamento após a reintegração.</p>
+						<p>Realizamos a cada dois anos um seminário estadual elegendo temas relacionados à Adoção e à institucionalização de crianças e adolescentes com o objetivo de debater e propor melhorias para os problemas que cerceiam o universo da Adoção.</p>
 						<a class="btn" href="<?php printf(home_url());?>/form" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Se inscreva</a>
 					</div>
 				</div>
@@ -168,9 +181,9 @@ get_header();
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2">
 					<div class="modal-body">
-						<h2>Papo da Adoção</h2>
+						<h2>Exposições Fotográficas</h2>
 						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/exposicao.jpg" alt="">
-						<p>Reunião com pretendentes e adotantes visando a preparação para o exercício da maternidade/paternidade. Sempre na última sexta-feira de cada mês, às 18 horas, na sede do Cria.</p>
+						<p>Também realizadas a cada dois anos, as exposições fotográficas são intervenções que visam sensibilizar e chamar a atenção da sociedade sobre temas, relacionados à adoção, que ainda são tabus.</p>
 						<a class="btn" href="<?php printf(home_url());?>/form" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Se inscreva</a>
 					</div>
 				</div>
@@ -179,29 +192,28 @@ get_header();
 	</div>
 </div>
 <!-- Modal 5 -->
-<!--<div class="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-hidden="true">
-<div class="modal-content">
-<div class="close-modal" data-dismiss="modal">
-<div class="lr">
-<div class="rl">
+<div class="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-content">
+		<div class="close-modal" data-dismiss="modal">
+			<div class="lr">
+				<div class="rl">
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="modal-body">
+						<h2>Projeto de Mãos Dadas</h2>
+						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/manha.jpg" alt="">
+						<p>Tem como objetivo fortalecer os laços entre os componentes do Sistema de Garantia de Direitos de Crianças e Adolescentes e, a partir disso, melhorar a qualidade de vida dentro dos abrigos.</p>
+						<a class="btn" href="<?php printf(home_url());?>/form" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Se inscreva</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-</div>
-</div>
-<div class="container">
-<div class="row">
-<div class="col-lg-8 col-lg-offset-2">
-<div class="modal-body">
-
-<h2>Manhã de Piscina</h2>
-<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/manha.jpg" alt="">
-<p>Momento de lazer com as crianças acolhidas no Abrigo Lar da Criança Maria João de Deus no clube da ADUFPI, realizado no último sábado de cada mês, no intuito de contribuir para a convivência comunitária destas crianças.</p>
-<button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Fechar</button>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>-->
 <!-- Modal 6 -->
 <div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">
 	<div class="modal-content">
@@ -215,9 +227,9 @@ get_header();
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2">
 					<div class="modal-body">
-						<h2>Adoções Necessárias</h2>
+						<h2>Campanha Abandonar é Crime</h2>
 						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/grupo.jpg" alt="">
-						<p>Busca ativa por famílias para adoção de grupos de irmãos, crianças especiais ou com deficiência, negros, crianças especiais ou com deficiência, negros, crianças maiores e adolescentes.</p>
+						<p>Visa evitar o abandono de crianças e enfatizar a diferença entre o abandono e a entrega consciente de um filho para adoção, sensibilizando a sociedade e desmistificando todo o processo que envolve a desistência legal do poder familiar.</p>
 						<a class="btn" href="<?php printf(home_url());?>/form" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Se inscreva</a>
 					</div>
 				</div>
--- page-templates/comoAjudar.php
+++ page-templates/comoAjudar.php
@@ -12,7 +12,7 @@ get_header();
 				<div class="container">
 					<div class="row">
 						<div class="col-lg-12 text-center">
-							<h2 class="section-heading">Como Ajudar</h2>
+							<h2 class="section-heading"><?php the_title(); ?></h2>
 							<hr class="primary">
 						</div>
 					</div>
