a911d1d72fd80130efebc919d7bb8df64e3b585e
Camila Vilarinho <camilaavilarinho@gmail.com>
2016-08-17 22:31:31 -0300

    Add images

--- page-templates/acoes.php
+++ page-templates/acoes.php
@@ -21,7 +21,7 @@ get_header();
 										<i class="fa fa-plus fa-3x"></i>
 									</div>
 								</div>
-								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/familia.png" class="img-responsive" alt="">
+								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/acoes/pintando.png" class="img-responsive" alt="">
 							</a>
 							<div class="portfolio-caption">
 								<h4>Pintando o Sete e Reunião com Famílias</h4>
@@ -34,7 +34,7 @@ get_header();
 										<i class="fa fa-plus fa-3x"></i>
 									</div>
 								</div>
-								<img src="<?php bloginfo('stylesheet_directory');?>/images/projetos/papo.png" class="img-responsive" alt="">
+								<img src="<?php bloginfo('stylesheet_directory');?>/images/projetos/acoes/manha.png" class="img-responsive" alt="">
 							</a>
 							<div class="portfolio-caption">
 								<h4>Manhã de Lazer</h4>
@@ -47,7 +47,7 @@ get_header();
 										<i class="fa fa-plus fa-3x"></i>
 									</div>
 								</div>
-								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/maosdadas.png" class="img-responsive" alt="">
+								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/acoes/seminario.png" class="img-responsive" alt="">
 							</a>
 							<div class="portfolio-caption">
 								<h4>Seminários Estaduais</h4>
@@ -60,7 +60,7 @@ get_header();
 										<i class="fa fa-plus fa-3x"></i>
 									</div>
 								</div>
-								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/exposicao.png" class="img-responsive" alt="">
+								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/acoes/exposicao.png" class="img-responsive" alt="">
 							</a>
 							<div class="portfolio-caption">
 								<h4>Exposições Fotográficas</h4>
@@ -73,7 +73,7 @@ get_header();
 										<i class="fa fa-plus fa-3x"></i>
 									</div>
 								</div>
-								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/exposicao.png" class="img-responsive" alt="">
+								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/acoes/maosdadas.png" class="img-responsive" alt="">
 							</a>
 							<div class="portfolio-caption">
 								<h4>Projeto de Mãos Dadas</h4>
@@ -86,7 +86,7 @@ get_header();
 										<i class="fa fa-plus fa-3x"></i>
 									</div>
 								</div>
-								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/grupo.png" class="img-responsive" alt="">
+								<img src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/acoes/abandonar.png" class="img-responsive" alt="">
 							</a>
 							<div class="portfolio-caption">
 								<h4>Campanha Abandonar é Crime</h4>
@@ -113,7 +113,7 @@ get_header();
 				<div class="col-lg-8 col-lg-offset-2">
 					<div class="modal-body">
 						<h2>Pintando o Sete e Reunião com Famílias</h2>
-						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/familia.jpg" alt="">
+						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/acoes/pintando.jpg" alt="">
 						<p>O <strong>O Pintando o Sete e Reunião com Famílias </strong> é um evento que acontece na última quarta-feira de cada mês e consiste em reunir as Famílias Acolhedoras que participam do programa do Cria acolhendo crianças ou adolescentes em suas casas. No Pintando o Sete as crianças e adolescente desfrutam de atividades e brincadeiras junto a um grupo de voluntários ou estagiários. Já a Reunião das Famílias é sempre mediada por nossa equipe psicossocial, abordando temas relevantes para o convívio no acolhimento familiar.</p>
 						<a class="btn" href="<?php printf(home_url());?>/form" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Se inscreva</a>
 					</div>
@@ -136,7 +136,7 @@ get_header();
 				<div class="col-lg-8 col-lg-offset-2">
 					<div class="modal-body">
 						<h2>Manhã de Lazer</h2>
-						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/papo.jpg" alt="">
+						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/acoes/manha.jpg" alt="">
 						<p>Também chamada de <strong>Manhã de Piscina</strong>, é um evento que acontece nos últimos sábados dos meses com o objetivo de proporcionar lazer e bem-estar às crianças e adolescentes que vivem em instituições de acolhimento de Teresina.</p>
 						<a class="btn" href="<?php printf(home_url());?>/form" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Se inscreva</a>
 					</div>
@@ -159,7 +159,7 @@ get_header();
 				<div class="col-lg-8 col-lg-offset-2">
 					<div class="modal-body">
 						<h2>Seminários Estaduais</h2>
-						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/maosdadas.jpg" alt="">
+						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/acoes/seminario.jpg" alt="">
 						<p>Realizamos a cada dois anos um seminário estadual elegendo temas relacionados à Adoção e à institucionalização de crianças e adolescentes com o objetivo de debater e propor melhorias para os problemas que cerceiam o universo da Adoção.</p>
 						<a class="btn" href="<?php printf(home_url());?>/form" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Se inscreva</a>
 					</div>
@@ -182,7 +182,7 @@ get_header();
 				<div class="col-lg-8 col-lg-offset-2">
 					<div class="modal-body">
 						<h2>Exposições Fotográficas</h2>
-						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/exposicao.jpg" alt="">
+						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/acoes/exposicao.jpg" alt="">
 						<p>Também realizadas a cada dois anos, as exposições fotográficas são intervenções que visam sensibilizar e chamar a atenção da sociedade sobre temas, relacionados à adoção, que ainda são tabus.</p>
 						<a class="btn" href="<?php printf(home_url());?>/form" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Se inscreva</a>
 					</div>
@@ -205,7 +205,7 @@ get_header();
 				<div class="col-lg-8 col-lg-offset-2">
 					<div class="modal-body">
 						<h2>Projeto de Mãos Dadas</h2>
-						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/manha.jpg" alt="">
+						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/acoes/maosdadas.jpg" alt="">
 						<p>Tem como objetivo fortalecer os laços entre os componentes do Sistema de Garantia de Direitos de Crianças e Adolescentes e, a partir disso, melhorar a qualidade de vida dentro dos abrigos.</p>
 						<a class="btn" href="<?php printf(home_url());?>/form" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Se inscreva</a>
 					</div>
@@ -228,7 +228,7 @@ get_header();
 				<div class="col-lg-8 col-lg-offset-2">
 					<div class="modal-body">
 						<h2>Campanha Abandonar é Crime</h2>
-						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/grupo.jpg" alt="">
+						<img class="img-responsive img-centered" src="<?php bloginfo('stylesheet_directory'); ?>/images/projetos/acoes/abandonar.jpg" alt="">
 						<p>Visa evitar o abandono de crianças e enfatizar a diferença entre o abandono e a entrega consciente de um filho para adoção, sensibilizando a sociedade e desmistificando todo o processo que envolve a desistência legal do poder familiar.</p>
 						<a class="btn" href="<?php printf(home_url());?>/form" role="button"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Se inscreva</a>
 					</div>
